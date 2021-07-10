7c771fa6df599d2a63af12a8ae65d4adfa19b3d7
diegolins10 <diegofernandeslins@gmail.com>
2021-07-10 16:07:23 -0300

    Add project files.

---
+++ Pedido/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Pedido.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0, // pedindo pagamentos
+        Processing = 1,     //processando
+        Shipped = 2,        // enviado
+        Delivered = 3       // entregue
+    }
+}
---
+++ Pedido/Program.cs
@@ -0,0 +1,24 @@
+﻿using Pedido.Entities;
+using Pedido.Entities.Enums;
+using System;
+
+namespace Pedido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order();
+            order.Id = 01;
+            order.Moment = DateTime.Now;
+            order.Status = OrderStatus.Processing;
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+            Console.WriteLine(order);
+        }
+    }
+}

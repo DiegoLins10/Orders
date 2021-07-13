59fd0f43b87639fd05e1bb62d8516e253367bc15
diegolins10 <diegofernandeslins@gmail.com>
2021-07-12 23:16:55 -0300

    Client class

---
+++ Pedido/Entities/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pedido.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}

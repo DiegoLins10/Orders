95be4f41e9036959188c7a9f932ea67dbbb91269
diegolins10 <diegofernandeslins@gmail.com>
2021-07-13 21:32:11 -0300

    Finishing program

--- Pedido/Program.cs
+++ Pedido/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Pedido.Entities;
 using Pedido.Entities.Enums;
 using System;
+using System.Globalization;
 
 namespace Pedido
 {
@@ -8,16 +9,60 @@ namespace Pedido
     {
         static void Main(string[] args)
         {
-            Order order = new Order();
-            order.Id = 01;
-            order.Moment = DateTime.Now;
-            order.Status = OrderStatus.Processing;
+            
+            /******** Populando construtor da classe cliente ****************/
+            Console.WriteLine("Enter client data");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth Date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Client c = new Client(name, email, birthDate);
 
-            string txt = OrderStatus.PendingPayment.ToString();
-            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            /******** Pegando o status do pedido do enum ****************/
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: (PendingPayment/Processing/Shipped/Delivered): ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine()); //chama o enum de status
 
-            Console.WriteLine(txt);
-            Console.WriteLine(os);
+            /******** Construtor da Order com nome dados dos clientes ****************/
+            Order order = new Order(DateTime.Now, status, c); // objeto order tem que ser criado fora do for
+
+            /******** Solicitando o numero de itens ****************/
+            Console.Write("How many items to this order? ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            
+            /******** adicionando e criando os objetos do pedido ****************/
+            for ( int i = 0; i<number; i++)
+            {
+                Console.WriteLine($"Enter #{i+1} item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Quantity: ");
+                int productQuantity = int.Parse(Console.ReadLine());
+
+                
+                Product p = new Product(productName, productPrice );
+                OrderItem orderItem = new OrderItem(productQuantity, productPrice, p);
+                order.AddItem(orderItem);
+
+            }
+
+            //order.Moment = DateTime.Now;
+            //order.Status = OrderStatus.Processing;
+
+            //string txt = OrderStatus.PendingPayment.ToString();
+            //OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            //Console.WriteLine(txt);
+            //Console.WriteLine(os);
+            //Order or = new Order();
+            Console.WriteLine();
             Console.WriteLine(order);
         }
     }
